0e6c283998549aa67e7cc1b2703f4dc99236c98c
ConstantineMichalis <66834305+ConstantineMichalis@users.noreply.github.com>
2021-08-10 07:20:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import plotly.express as px
 import flask
 import dash; import dash_core_components as dcc; import dash_html_components as html
 from dash.dependencies import Input, Output
-
+import cx_Oracle
+import sys
+ 
 # Setup the app. The server & app names should match those in Procfile 
 app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 
@@ -11,8 +13,22 @@ app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=devi
 server = app.server
 
 
-dts = pd.read_csv('./Locationregions.csv', sep= ',')
-lab = pd.read_csv('./laborregions.csv', sep= ',')
+#dts = pd.read_csv('./Locationregions.csv', sep= ',')
+#lab = pd.read_csv('./laborregions.csv', sep= ',')
+
+dsn_tns = cx_Oracle.makedsn('jllorclprd.cmiptbjmy4tj.eu-west-1.rds.amazonaws.com', '1521', service_name='ORCL') 
+conn = cx_Oracle.connect(user=r'CONSTANTINE_MICHALIS', password=r'Qror25c_H85o', dsn=dsn_tns) 
+
+dts = conn.cursor()
+dts.execute("""select distinct l.location, pluspcustomer,   (select name from pluspcustomer where customer = pluspcustomer) name,  s.latitudey, s.longitudex, a.ancestor, a2.ancestor as branch, c.type, l.status, s.postalcode
+from locations l
+join serviceaddress s on saddresscode = addresscode
+join locancestor a on a.location = l.location
+join locancestor a2 on a2.location = l.location
+join pluspcustomer c on c.customer = l.pluspcustomer
+where (a.ancestor not like ('L%') and a.ancestor not like ('%00000%') and a2.ancestor not like ('L%') and a2.ancestor like ('%00000%') and a2.ancestor != '000000')-- and longitudex < -5.5 and a.ancestor = (select parent from lochierarchy where location = l.location)
+and (pluspcustomer in (select customer from pluspagreement where enddate>sysdate) and c.status = 'ACTIVE') 
+order by ancestor, pluspcustomer, location;""") # use triple quotes if you want to spread your query across multiple lines
 
 
 dts = dts[dts['PLUSPCUSTOMER'] != 'C1000238']
